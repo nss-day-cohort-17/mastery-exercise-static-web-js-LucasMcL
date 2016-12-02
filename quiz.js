72b730dacb5d2a7c72da71d64a7e2c519c02400b
LucasMcL <LMcLaughlin09@gmail.com>
2016-12-02 13:34:06 -0600

    wrote out overall objectives and structure

---
+++ quiz.js
@@ -0,0 +1,15 @@
+// Overall Objectives
+
+// Create object from user input
+// Create function that reads values from object to create tree in console
+// Check to see if both fields have values in them
+  // Do this within the function?  Elsewhere?
+
+// Additional: check to see if tree height input is number
+// Additional: check to see if character input is a length of 1
+
+//
+
+
+
+
